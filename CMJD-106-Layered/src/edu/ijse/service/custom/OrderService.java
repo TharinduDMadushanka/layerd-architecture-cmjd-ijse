09600ae968ab248f0220564124a0675d6c027dbe
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-28 08:39:11 +0530

    layered project

---
+++ CMJD-106-Layered/src/edu/ijse/service/custom/OrderService.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package edu.ijse.service.custom;
+
+import edu.ijse.service.SuperService;
+
+/**
+ *
+ * @author ASUS
+ */
+public interface OrderService extends SuperService{
+    
+}
