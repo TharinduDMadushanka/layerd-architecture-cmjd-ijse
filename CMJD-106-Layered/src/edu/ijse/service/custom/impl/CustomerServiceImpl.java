98ede12b9c9a4083c1e2362048ad5759f4c9bb41
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-29 11:55:30 +0530

    customer dao impl

--- CMJD-106-Layered/src/edu/ijse/service/custom/impl/CustomerServiceImpl.java
+++ CMJD-106-Layered/src/edu/ijse/service/custom/impl/CustomerServiceImpl.java
@@ -4,7 +4,10 @@
  */
 package edu.ijse.service.custom.impl;
 
+import edu.ijse.dao.DaoFactory;
+import edu.ijse.dao.custom.CustomerDao;
 import edu.ijse.dto.CustomerDto;
+import edu.ijse.entity.CustomerEntity;
 import edu.ijse.service.custom.CustomerService;
 import java.util.ArrayList;
 
@@ -13,6 +16,8 @@ import java.util.ArrayList;
  * @author ASUS
  */
 public class CustomerServiceImpl implements CustomerService{
+    
+    private CustomerDao CustomerDao = (CustomerDao) DaoFactory.getInstance().getDao(DaoFactory.DaoTypes.CUSTOMER);
 
     @Override
     public String save(CustomerDto customerDto) throws Exception {
@@ -30,6 +35,19 @@ public class CustomerServiceImpl implements CustomerService{
     }
     @Override
     public CustomerDto getCustomer(String custId) throws Exception {
+        CustomerEntity entity = CustomerDao.get(custId);
+        if(entity != null){
+            CustomerDto dto = new CustomerDto(entity.getId(),
+                    entity.getTitle(),
+                    entity.getName(),
+                    entity.getDob(),
+                    entity.getSalary(),
+                    entity.getAddress(),
+                    entity.getCity(),
+                    entity.getProvince(),
+                    entity.getPostal());
+            return dto;
+        }
         return null;
     }
 
