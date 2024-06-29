642e3632cbac54f777a32ef1d45331f7367fca6d
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-29 08:50:05 +0530

    layered project

--- CMJD-106-Layered/src/edu/ijse/dao/SuperDao.java
+++ CMJD-106-Layered/src/edu/ijse/dao/SuperDao.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package edu.ijse.dao;
 
@@ -8,6 +8,6 @@ package edu.ijse.dao;
  *
  * @author ASUS
  */
-public class SuperDao {
+public interface SuperDao {
     
 }
