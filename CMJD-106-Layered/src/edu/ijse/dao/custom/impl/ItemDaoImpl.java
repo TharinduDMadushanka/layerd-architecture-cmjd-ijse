fff8c2c7df137c865e768aa238918407d80be3b8
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-29 09:08:22 +0530

    layered project

---
+++ CMJD-106-Layered/src/edu/ijse/dao/custom/impl/ItemDaoImpl.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.ijse.dao.custom.impl;
+
+import edu.ijse.dao.custom.ItemDao;
+import edu.ijse.entity.ItemEntity;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ItemDaoImpl implements ItemDao{
+
+    @Override
+    public boolean create(ItemEntity t) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean update(ItemEntity t) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean delete(String id) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ItemEntity get(String id) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ArrayList<ItemEntity> getAll() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
